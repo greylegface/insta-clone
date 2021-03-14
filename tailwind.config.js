72fa931f55085aac8c63727c27da6047346c4126
Chris <chris@builtypixel.com>
2021-03-14 20:46:24 +0000

    added purge to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    mode: 'all',
+    preserveHtmlElements: false,
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     fill: (theme) => ({
